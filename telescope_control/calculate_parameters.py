f2a65edbda4e124d33c7deaad15e9016917def1b
Felipe Schubert Costa <schubert.cubo@gmail.com>
2021-11-13 14:20:48 +0100

    improving code and fixing problem with last position

--- telescope_control/calculate_parameters.py
+++ telescope_control/calculate_parameters.py
@@ -3,72 +3,79 @@ import utils
 from sympy import *
 import constants
 import charts
+import numpy as np
 
-def calculate_parameters(az, al):   
-    # Calculating Transformations for the robot
-    robot  = variables.get_robot_variables()
-    qs = symbols('q1:3')
-    R10 = utils.rotate_matrix('z', qs[0])
-    D10 = utils.position_matrix(robot["l1"])
-    T10 = R10*D10
-    
-    R21 = utils.rotate_matrix('y', -qs[1])
-    D21 = utils.position_matrix(robot["l2"])
-    T21 = R21*D21
-    T20 = T10*T21
+# Calculating Transformations for the robot
+robot  = variables.get_robot_variables()
+qs = symbols('q1:3')
+R10 = utils.rotate_matrix('z', qs[0])
+D10 = utils.position_matrix(robot["l1"])
+T10 = R10*D10
 
+R21 = utils.rotate_matrix('y', -qs[1])
+D21 = utils.position_matrix(robot["l2"])
+T21 = R21*D21
+T20 = T10*T21
+
+# Calculating Transformations for the task
+Als = Symbol('Al')
+Azs = Symbol('Az')
+R50 = utils.rotate_matrix('z', Azs)
+D50 = utils.position_matrix([0, 0, 0, 1])
+T50 = R50*D50
+
+R65 = utils.rotate_matrix('y', -Als)
+D65 = utils.position_matrix([0, 0, 0, 1])
+T65 = R65*D65
+
+rs = Symbol('r')
+T76 = eye(4)*utils.position_matrix([rs, 0, 0, 1])
+T75 = T50*T65*T76
+
+# Calculating equation for q1 and q2
+q1s = Azs
+
+l1s = robot["l1"][2]
+l2s = robot["l2"][0]
+
+A = (l2s/cos(Als))**2
+B = l2s**2 + l1s**2
+C = 2*l1s*l2s
+D = -1 + B/A
+E = C/A
+
+q2s = [asin((-E + sqrt(E**2 - 4*D))/2),  asin((-E - sqrt(E**2 - 4*D))/2)]
+
+# That is the telescope last position
+last_position = constants.initial_position
+
+def calculate_parameters(az, al):
+    global last_position
+    print("last position -> %s" % last_position)
+    print("target position -> %s" % [az, al])
+    # Calculating rotation in y axis
     R32 = utils.rotate_matrix('y', -(al - qs[1]))
     D32 = utils.position_matrix([0, 0, 0, 1])
     T32 = R32*D32
 
     T30 = T20*T32
-
-    # Calculating Transformations for the task
-    Als = Symbol('Al')
-    Azs = Symbol('Az')
-    R50 = utils.rotate_matrix('z', Azs)
-    D50 = utils.position_matrix([0, 0, 0, 1])
-    T50 = R50*D50
-    
-    R65 = utils.rotate_matrix('y', -Als)
-    D65 = utils.position_matrix([0, 0, 0, 1])
-    T65 = R65*D65
-
-    rs = Symbol('r')
-    T76 = eye(4)*utils.position_matrix([rs, 0, 0, 1])
-    T75 = T50*T65*T76
-
-    # Calculating equation for q1 and q2
-    q1s = Azs
-
-    l1s = robot["l1"][2]
-    l2s = robot["l2"][0]
-
-    A = (l2s/cos(Als))**2
-    B = l2s**2 + l1s**2
-    C = 2*l1s*l2s
-    D = -1 + B/A
-    E = C/A
-
-    q2s = [asin((-E + sqrt(E**2 - 4*D))/2),  asin((-E - sqrt(E**2 - 4*D))/2)]
-
     # loop start   
     T75_radius_adapted = T75.subs([(rs, T30[0:3,3].norm())])
 
-    angles =  utils.get_parametrization(constants.current_position, [az, al], constants.time_for_each_moviment, constants.steps)
+    angles =  utils.get_parametrization(last_position, [az, al], constants.time_for_each_moviment, constants.steps)
     positions = []
     q = []
 
     for index, next_intermediate_angle in enumerate(angles):
         q1 = get_best_q1(q1s, [(Azs, next_intermediate_angle[0])])
-        q2 = get_best_q2(q2s, [(l1s, constants.l1), (l2s, constants.l2), (Als, next_intermediate_angle[1])], constants.current_position[1] if index == 0 else angles[index-1][1])
+        q2 = get_best_q2(q2s, [(l1s, constants.l1), (l2s, constants.l2), (Als, next_intermediate_angle[1])], last_position[1] if index == 0 else angles[index-1][1])
         new_position = [q1, q2]
         q.append(new_position)
-        constants.current_position = new_position
+        last_position = next_intermediate_angle
 
         R75 = T75_radius_adapted[0:3,3].subs([(l1s, constants.l1), (l2s, constants.l2), (Als, next_intermediate_angle[1]), (Azs, next_intermediate_angle[0]), (qs[0], q1), (qs[1], q2)])
         positions.append(R75)
-
+    print(angles)
     charts.plot_chart(positions)
 
 def get_best_q1(q1s, sub_params):
@@ -112,4 +119,4 @@ def get_best_q2(q2s, sub_params, previous_q2):
     exit()
     
 if __name__ == "__main__":
-    calculate_parameters()
+    calculate_parameters(3.14,3.14/4)
--- telescope_control/constants.py
+++ telescope_control/constants.py
@@ -9,5 +9,5 @@ steps = 10
 q1_limit = [0.0, 2*pi]
 # Limit pi/2 chose to keep always the value of Az = q1 true
 q2_limit = [-pi/4, pi/2]
-current_position = [0.0, atan(l1/l2)]
+initial_position = [0.0, atan(l1/l2)]
 max_tolerated_imaginary = 0.00001
